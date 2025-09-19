78ee3d4f084d389e1b06f4e76866c708f98183d6
INGENIO <ingeniokhalif@gmail.com>
2025-09-19 12:04:57 +0100

    Initial commit

---
+++ src/main/java/app/vercel/ingenio_theta/haala/HaalaApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.ingenio_theta.haala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/vercel/ingenio_theta/haala/HaalaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.ingenio_theta.haala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
