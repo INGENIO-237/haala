d8449fee0829234f78b9cf58b2fee357e09b68ac
INGENIO <ingeniokhalif@gmail.com>
2025-09-23 11:15:48 +0100

    feat: enable JPA auditing for User entity; add createdAt and updatedAt fields with auditing annotations

--- src/main/java/app/vercel/ingenio_theta/haala/HaalaApplication.java
+++ src/main/java/app/vercel/ingenio_theta/haala/HaalaApplication.java
@@ -2,8 +2,10 @@ package app.vercel.ingenio_theta.haala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HaalaApplication {
 
 	public static void main(String[] args) {
